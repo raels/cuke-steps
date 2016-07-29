30b44684a594b53035501cfe29ef6251648fafa9
Eric Kiebler <eric.kiebler@onelogin.com>
2016-07-29 08:37:11 -0700

    added rudimentary specs

--- spec/cuke_steps_gem_spec.rb
+++ spec/cuke_steps_gem_spec.rb
@@ -5,7 +5,23 @@ describe CukeStepsGem do
     expect(CukeStepsGem::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context "html generation" do
+    before do
+      system 'rm steps.html'
+      system 'bundle exec cuke-steps .'
+    end
+    it 'generates a spec.html file' do
+      expect(File.exist?('steps.html')).to eq(true)
+    end
+  end
+
+  context "html generation" do
+    before do
+      system 'rm steps.cf'
+      system 'bundle exec cuke-steps -f cf .'
+    end
+    it 'generates a spec.html file' do
+      expect(File.exist?('steps.cf')).to eq(true)
+    end
   end
 end
